e49f36018027fa5a3cb0461c17e26414c416f6cf
Leonardo Torres Tellez <hobitse1@gmail.com>
2025-11-11 20:40:44 -0600

    Ejercicio 15 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -245,7 +245,13 @@ function promedioArray(numeros) {
 function encontrarMaximo(numeros) {
   // TODO: Encuentra y retorna el nÃºmero mÃ¡s grande
   // Pista: Puedes usar Math.max(...numeros) o hacerlo con un bucle
-  return 0;
+  let maximo = numeros[0];
+  for (let i = 1; i < numeros.length; i++) {
+    if (numeros[i] > maximo) {
+      maximo = numeros[i];
+    }
+  }
+  return maximo;
 }
 
 /**
