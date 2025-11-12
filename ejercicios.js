1325a7a6bd844791f8e827522699a130e0431632
Leonardo Torres Tellez <hobitse1@gmail.com>
2025-11-11 20:47:06 -0600

    Ejercicio 18 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -295,7 +295,14 @@ function invertirArray(arr) {
  */
 function crearMatriz(filas, columnas) {
   // TODO: Crea una matriz de filas x columnas llena de ceros
-  const matriz = [];
+   const matriz = [];
+  for (let i = 0; i < filas; i++) {
+    const fila = [];
+    for (let j = 0; j < columnas; j++) {
+      fila.push(0);
+    }
+    matriz.push(fila);
+  }
   return matriz;
 }
 
