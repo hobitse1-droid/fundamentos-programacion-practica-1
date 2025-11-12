1a3c01dc363eb14ec606723cdc4bc68c65bf19ef
Leonardo Torres Tellez <hobitse1@gmail.com>
2025-11-11 20:51:24 -0600

    Ejercicio 21 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -346,6 +346,9 @@ function obtenerFila(matriz, indiceFila) {
 function obtenerColumna(matriz, indiceColumna) {
   // TODO: Extrae todos los elementos de la columna indicada
   const columna = [];
+  for (let i = 0; i < matriz.length; i++) {
+    columna.push(matriz[i][indiceColumna]);
+  }
   return columna;
 }
 
